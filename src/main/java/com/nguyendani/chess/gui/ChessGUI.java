ff4b43e28b7b2c106ee0d5c37a2a57f009f4d5fd
Daniel Nguyen <nguyendaniel1997@gmail.com>
2024-01-09 20:08:20 -0600

    Added standard piece placement

--- src/main/java/com/nguyendani/chess/gui/ChessGUI.java
+++ src/main/java/com/nguyendani/chess/gui/ChessGUI.java
@@ -26,15 +26,47 @@ public class ChessGUI extends JFrame {
         chessboardPanel = new JPanel(new GridLayout(8, 8));
         boolean isWhite = false;
 
+            // Array to store piece types and colors for the initial board setup
+        String[][] initialBoard = {
+            {"blackRook", "blackKnight", "blackBishop", "blackQueen", "blackKing", "blackBishop", "blackKnight", "blackRook"},
+            {"blackPawn", "blackPawn", "blackPawn", "blackPawn", "blackPawn", "blackPawn", "blackPawn", "blackPawn"},
+            {"", "", "", "", "", "", "", ""},
+            {"", "", "", "", "", "", "", ""},
+            {"", "", "", "", "", "", "", ""},
+            {"", "", "", "", "", "", "", ""},
+            {"whitePawn", "whitePawn", "whitePawn", "whitePawn", "whitePawn", "whitePawn", "whitePawn", "whitePawn"},
+            {"whiteRook", "whiteKnight", "whiteBishop", "whiteQueen", "whiteKing", "whiteBishop", "whiteKnight", "whiteRook"}
+        };
+
         for (int i = 0; i < 8; i++) {
             isWhite = !isWhite; // flip color for each row
             for (int j = 0; j < 8; j++) {
                 JButton square = new JButton();
                 square.setBackground(isWhite ? Color.WHITE : Color.BLACK);
+
+                String pieceName = initialBoard[i][j];
+
+                if (!pieceName.isEmpty()) {
+                    // Load piece image based on the piece type and color
+                    ImageIcon pieceIcon = loadImageIcon(pieceName + ".png");
+                    square.setIcon(pieceIcon);
+                }
+
                 isWhite = !isWhite; // flip color for the next square
                 square.addActionListener(new ChessboardButtonListener());
                 chessboardPanel.add(square);
             }
+        }
+    }
+
+    private ImageIcon loadImageIcon(String imageName) {
+        ClassLoader classLoader = getClass().getClassLoader();
+        java.net.URL imageUrl = classLoader.getResource("images/" + imageName);
+        if (imageUrl != null) {
+            return new ImageIcon(imageUrl);
+        } else {
+            // Handle the case where the image is not found
+            return new ImageIcon(); // Default empty icon
         }
     }
 

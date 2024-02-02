364237a36662d51d171ce381c75ef7512921c8c3
Daniel Nguyen <nguyendaniel1997@gmail.com>
2024-02-01 22:03:55 -0600

    Reformat GUI

--- src/main/java/com/nguyendani/chess/gui/ChessGUI.java
+++ src/main/java/com/nguyendani/chess/gui/ChessGUI.java
@@ -1,15 +1,11 @@
 package com.nguyendani.chess.gui;
 
-import com.nguyendani.chess.pieces.*;
-
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class ChessGUI extends JFrame {
-    private JPanel chessboardPanel;
-    private JButton[][] squares;
+    private JPanel chessBoard;
+    private JButton[][] chessTile;
 
     public ChessGUI() {
         setTitle("Chess Game");
@@ -19,77 +15,35 @@ public class ChessGUI extends JFrame {
         setLayout(new BorderLayout());
 
         createChessboard();
-        setBoard();
-        createNewGame();
+        newGameButton();
         
         setVisible(true);
     }
 
     // Chessboard
     private void createChessboard() {
-        chessboardPanel = new JPanel(new GridLayout(8, 8));
+        chessBoard = new JPanel(new GridLayout(8, 8));
+        chessTile = new JButton[8][8];
         boolean isWhite = true;
-        squares = new JButton[8][8];
+        
+        for(int row = 0; row < 8; row++){
+            for(int col = 0; col < 8; col++){
+                JButton tile = new JButton();
+                tile.setFocusPainted(false);
 
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                JButton square = new JButton();
-                // Tile Colors
-                square.setBackground(isWhite ? Color.WHITE : Color.BLACK);
+                tile.setBackground(isWhite ? Color.WHITE : Color.BLACK);
 
                 isWhite = !isWhite;
-                chessboardPanel.add(square);
-                squares[i][j] = square;
+                chessBoard.add(tile);
+                chessTile[row][col] = tile;
             }
             isWhite = !isWhite;
         }
-
-        add(chessboardPanel, BorderLayout.CENTER);
-    }
-
-    private void setBoard() {
-        // Array to store piece types and colors for the initial board setup
-        String[][] initialBoard = {
-            {"blackRook", "blackKnight", "blackBishop", "blackQueen", "blackKing", "blackBishop", "blackKnight", "blackRook"},
-            {"blackPawn", "blackPawn", "blackPawn", "blackPawn", "blackPawn", "blackPawn", "blackPawn", "blackPawn"},
-            {"", "", "", "", "", "", "", ""},
-            {"", "", "", "", "", "", "", ""},
-            {"", "", "", "", "", "", "", ""},
-            {"", "", "", "", "", "", "", ""},
-            {"whitePawn", "whitePawn", "whitePawn", "whitePawn", "whitePawn", "whitePawn", "whitePawn", "whitePawn"},
-            {"whiteRook", "whiteKnight", "whiteBishop", "whiteQueen", "whiteKing", "whiteBishop", "whiteKnight", "whiteRook"}
-        };
-
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                String pieceName = initialBoard[i][j];
-                if (!pieceName.isEmpty()) {
-                    // Load piece image based on the piece type and color
-                    ImageIcon pieceIcon = loadImageIcon(pieceName + ".png");
-                    squares[i][j].setIcon(pieceIcon);
-                    new Queen(true, i, j, pieceIcon);
-                }
-                else {
-                    squares[i][j].setIcon(null);
-                }
-            }
-        }
-    }
-
-    // Load Image
-    private ImageIcon loadImageIcon(String imageName) {
-        ClassLoader classLoader = getClass().getClassLoader();
-        java.net.URL imageUrl = classLoader.getResource("images/" + imageName);
-        if (imageUrl != null) {
-            return new ImageIcon(imageUrl);
-        } else {
-            // Handle the case where the image is not found
-            return new ImageIcon(); // Default empty icon
-        }
+        add(chessBoard, BorderLayout.CENTER);
     }
 
     // New Game Button
-    private void createNewGame() {
+    private void newGameButton() {
         JPanel controlPanel = new JPanel();
         JButton newGameButton = new JButton("New Game");
         controlPanel.add(newGameButton);
@@ -97,13 +51,7 @@ public class ChessGUI extends JFrame {
         add(controlPanel, BorderLayout.SOUTH);
     }
 
-    public static void main(String[] args) {
-        try {
-            UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    
-        SwingUtilities.invokeLater(() -> new ChessGUI());
+    public JButton[][] getChessTile() {
+        return chessTile;
     }
 }
